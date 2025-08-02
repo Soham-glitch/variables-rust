e69e1fd7c6ddf80d6094adb92e7f0a2454c3ff2d
Soham <sohamsinha2001@gmail.com>
2025-08-02 13:15:52 +0530

    second_commit

--- src/main.rs
+++ src/main.rs
@@ -24,5 +24,20 @@ fn main() {
         println!("the value in the scope is :{something}");
     }
     println!("the value out of the scope is : {something}");
+
+    //compound data types
+    
+    //tuples
+    let tup  = (-500,6.4,76);
+   let (x,y,z)=tup;
+   println!("{y}");
+   
+   //arrays
+   let _arr = [1,2,3,4,5,6,7,8];
+   let months = ["January", "February", "March", "April", "May", "June", "July",
+   "August", "September", "October", "November", "December"];
+   let first_month = months[0];
+
+   println!("first month is:{first_month}");
 }
 
